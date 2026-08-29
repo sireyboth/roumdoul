eb269557eeb6a96e73b0f68f2431d494b172a6fa
LANN Phorlly <lannphorlly.698896@gmail.com>
2026-08-29 14:58:29 +0700

    add storage S3 R2 with Laravel Cloud

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -35,6 +35,7 @@ class AdminPanelProvider extends PanelProvider
             ->colors([
                 'primary' => Color::hex('#b02361'),
             ])
+            ->spa(hasPrefetching: true)
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
             ->pages([
